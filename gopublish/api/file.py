621109d430e676085e64731986bcfa57412cc116
mboudet <mateo.boudet@gmail.com>
2021-03-03 11:12:17 +0000

    Fix contact

--- gopublish/api/file.py
+++ gopublish/api/file.py
@@ -167,7 +167,7 @@ def publish_file():
         contact = request.json['contact']
         try:
             v = validate_email(contact)
-            contact = [v["email"]]
+            contact = v["email"]
         except EmailNotValidError as e:
             return make_response(jsonify({'error': str(e)}), 400)
 
